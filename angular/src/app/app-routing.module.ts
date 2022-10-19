b07101dcb04aeb395d9393ec3b1a0713859e7156
faisalkhanafk84 <faisalkhanafk84@gmail.com>
2022-10-19 19:15:23 +0530

    routing,components generated,images added,

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CreateaccountComponent } from './components/createaccount/createaccount.component';
+import { ForgetpasswordComponent } from './components/forgetpassword/forgetpassword.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  // {path:'login',component:LoginComponent,pathMatch:'full'},
+  {path:'app',component:AppComponent},
+  {path:'forgetpassword',component:ForgetpasswordComponent},
+  {path:'createaccount',component:CreateaccountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ForgetpasswordComponent } from './components/forgetpassword/forgetpassword.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreateaccountComponent } from './components/createaccount/createaccount.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForgetpasswordComponent,
+    LoginComponent,
+    CreateaccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
